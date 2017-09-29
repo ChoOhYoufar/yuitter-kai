cade3164bfda113789b1d49a699bacfb4777c24f
YuitoSatoBiz <yuito.sato@bizreach.co.jp>
2017-09-29 14:29:28 +0900

    : )

--- yuitter-client/src/app/app.module.ts
+++ yuitter-client/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {
+  MdButtonModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdIconModule,
+  MdMenuModule,
+  MdToolbarModule,
+  MdInputModule,
+  MdChipsModule,
+  MdTabsModule,
+  MdSelectModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
