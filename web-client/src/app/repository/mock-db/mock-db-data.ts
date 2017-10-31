9acf94f2f0a2c77162572528be3a0bc5ea15afee
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2017-11-01 08:52:17 +0900

    一応コメントアウトしとく

--- web-client/src/app/repository/mock-db/mock-db-data.ts
+++ web-client/src/app/repository/mock-db/mock-db-data.ts
@@ -15,7 +15,7 @@ export class MockDbData implements InMemoryDbService {
           {
             tweetText: 'おはよう',
             imageUrl: 'https://www.google.co.jp/search?q=%E3%82%AF%E3%82%B8%E3%83%A9&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjl_M6d_IPXAhVGW7wKHcjkD5QQ_AUICigB&biw=1440&bih=799#',
-            registerDatetime: new DateTimeFormat,
+            registerDatetime: '2017-03-08 05:35:57',
             account: {
               accountName: 'テストアカ1',
               avatar: 'https://www.google.co.jp/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi3_7vj_IPXAhVGQLwKHZkkDLwQjRwIBw&url=https%3A%2F%2Fmatome.naver.jp%2Fodai%2F2142396408839928901&psig=AOvVaw1PbuYjKlYDY4ReO48RPnIL&ust=1508752880091664'
@@ -24,7 +24,7 @@ export class MockDbData implements InMemoryDbService {
           {
             tweetText: 'おはよう',
             imageUrl: 'https://www.google.co.jp/search?q=%E3%82%AF%E3%82%B8%E3%83%A9&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjl_M6d_IPXAhVGW7wKHcjkD5QQ_AUICigB&biw=1440&bih=799#',
-            registerDatetime: new DateTimeFormat,
+            registerDatetime: '2017-03-08 05:35:57',
             account: {
               accountName: 'テストアカ1',
               avatar: 'https://www.google.co.jp/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi3_7vj_IPXAhVGQLwKHZkkDLwQjRwIBw&url=https%3A%2F%2Fmatome.naver.jp%2Fodai%2F2142396408839928901&psig=AOvVaw1PbuYjKlYDY4ReO48RPnIL&ust=1508752880091664'
