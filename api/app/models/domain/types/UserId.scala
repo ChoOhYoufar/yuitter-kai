cb33fce8a7ea6c89a58d8e2da0e5ad93e5300b8e
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2017-11-15 19:15:04 +0900

    :  )

--- api/app/models/domain/types/UserId.scala
+++ api/app/models/domain/types/UserId.scala
@@ -2,15 +2,12 @@ package models.domain.types
 
 import models.domain.Iso
 
-case class UserId[A](value: Long) extends AnyVal {}
+case class UserId(value: Long) extends AnyVal {}
 
 object UserId {
 
-  implicit def userIdIso[A]: Iso[Long, UserId[A]] = new Iso[Long, UserId[A]] {
-    def to(value: Long): UserId[]  = UserId(value)
-    def from(model: UserId[A]):
+  implicit def userIdIso: Iso[Long, UserId] = new Iso[Long, UserId] {
+    def to(value: Long): UserId  = UserId(value)
+    def from(model: UserId): Long = model.value
   }
-
 }
-
-
