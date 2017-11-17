b731842bbc1792afda108a17592abd26ba647de7
YuitoSatoBiz <yuito.sato@bizreach.co.jp>
2017-11-18 02:07:04 +0900

    : )

--- api/app/models/domain/Iso.scala
+++ api/app/models/domain/Iso.scala
@@ -1,11 +1,5 @@
 package models.domain
 
-/**
-  * TODO: yuito あとでちゃんと調べて使う。いまはまだ難しい
-  * 現状はミックスインさせて実装方法を提示するくらいしか意味がない
-  * @tparam A 値型
-  * @tparam B ラップ型
-  */
 trait Iso[A, B] {
 
   def to(a: A): B
