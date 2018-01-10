310f035611bbd386567df9a21c57f7229cfdffaf
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2018-01-11 08:37:56 +0900

    : )

--- web-client/src/app/components/sign-up-form/sign-up-form-component.service.ts
+++ web-client/src/app/components/sign-up-form/sign-up-form-component.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AuthInfo } from '../../models/user/auth-info';
 import { UserHttpService } from '../../repositories/http/user/user-http.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class SignUpFormComponentService {
 
-  constructor(private userHttp: UserHttpService) {}
+  constructor(
+    private userHttp: UserHttpService,
+    private router: Router,
+  ) {}
 
   signUp(authInfo: AuthInfo): Promise<void> {
     return this.userHttp.signUp(authInfo);
+  }
+
+  // NOTE yuito sign inと同じロジックだから抽象化したい
+  canActivate(): void {
+    this.userHttp.getLoginStatus().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 }
--- web-client/src/app/components/sign-up-form/sign-up-form.component.ts
+++ web-client/src/app/components/sign-up-form/sign-up-form.component.ts
@@ -19,6 +19,7 @@ export class SignUpFormComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.service.canActivate();
     this.form = this.createForm();
   }
 
